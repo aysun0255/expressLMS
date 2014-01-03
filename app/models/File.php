caa711da9dc379af1371d123b70e7772b94a0471
User <User@pc>
2014-01-03 12:34:14 +0200

    new add

---
+++ app/models/File.php
@@ -0,0 +1,13 @@
+<?php
+
+class File extends Eloquent {
+
+    public $timestamps = true;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'files';
+}
