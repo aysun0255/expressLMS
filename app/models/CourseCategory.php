8218ca70dfbd2204268b62faebc6183cdcba3b3d
User <User@pc>
2014-01-03 15:19:32 +0200

    new add

---
+++ app/models/CourseCategory.php
@@ -0,0 +1,14 @@
+<?php
+
+class CourseCategory extends Eloquent {
+
+    public $timestamps = false;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'course_categories';
+
+}
