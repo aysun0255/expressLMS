370b9c6203b7dca965f3e5bdd85cb5c29d0bc598
User <User@pc>
2014-01-03 16:01:42 +0200

    new add

--- app/views/courses/create.blade.php
+++ app/views/courses/create.blade.php
@@ -11,6 +11,10 @@
         Form::textarea('description')}}
         {{$errors->first('description');}}
         </br>
+        {{Form::label('category','Category:'),
+        Form::select('category',array('not' => 'Not Selected',CourseCategory::lists('name', 'id')))}}
+        {{$errors->first('category');}}
+        </br>
         {{Form::submit('Create Course',['type' => 'button','class' => 'btn btn-lg btn-primary btn-block'])}}
     {{Form::close()}}
 @stop
