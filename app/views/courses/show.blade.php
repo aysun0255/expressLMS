5cd468c17d1fa988ce461bdb00b771295edd2ef1
User <User@pc>
2014-01-03 17:57:52 +0200

    new add

--- app/views/courses/show.blade.php
+++ app/views/courses/show.blade.php
@@ -6,8 +6,22 @@
     </br>
     <div class="right">
         <a href ="#">Add lesson</a>
-        <a href ="{{route('courses.edit', $course->id)}}">Edit course</a>
-        <a href ="#">Delete course</a>
+        <a href ="{{route('courses.edit', $course->id)}}" class ="btn btn-info">Edit course</a>
+        <?php
+            $usergroupId = Auth::user()->usergroup_id;
+            $usergroup = Usergroup::whereId($usergroupId)->first();
+            $canDeleteCourses = $usergroup->can_delete_courses;
+            $canEditCourses = $usergroup->can_edit_courses;
+            ?>
+            @if($canEditCourses =='yes')
+            @endif
+            @if($canDeleteCourses =='yes')
+
+                {{Form::open(array('route' => array('courses.destroy', $course->id),'method'=>'delete','style' =>'display:inline;'))}}
+                {{Form::input('hidden','course_id',$course->id)}}
+                {{Form::submit('Delete',['type' => 'button','class' => 'btn btn-info','onclick' => "return confirm('Are you sure?');"])}}
+                {{Form::close()}} 
+            @endif
     </div>
     {{$course->description}}
     </br>
