e7a47d1d89421208e67c7fc163fb92f70c42711b
User <User@pc>
2014-01-03 21:07:21 +0200

    new add

--- app/views/courses/enrol.blade.php
+++ app/views/courses/enrol.blade.php
@@ -5,6 +5,9 @@
     <div class="container">
         <center> 
             {{Form::open(array('route' => array('courses.enrol', $course->id),'method'=>'post','style' =>'display:inline;'))}}
+                @if($course->use_key == 'yes')
+                    {{Form::label('key','Enrolment key:'),Form::input('text','key')}}
+                @endif
                 {{Form::input('hidden','course_id',$course->id)}}
                 {{Form::submit('Enrol me in this course ',['type' => 'button','class' => 'btn btn-info','onclick' => "return confirm('Are you sure?');"])}}
             {{Form::close()}} 
