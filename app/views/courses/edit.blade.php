642ccb39efc4d63a10fd4327cd1e04ce0cb228b1
User <User@pc>
2014-01-03 10:55:21 +0200

    new add

---
+++ app/views/courses/edit.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>Edit Course</h1>
+    {{Form::open(array('route'=>'courses.store','class' => 'form-signin'))}}
+        {{Form::label('name','Course Name:'),
+        Form::input('text','name')}}
+        {{$errors->first('name');}}
+        </br>
+        {{Form::label('description','Description:'),
+        Form::textarea('description')}}
+        {{$errors->first('description');}}
+        </br>
+        {{Form::submit('Create Course',['type' => 'button','class' => 'btn btn-lg btn-primary btn-block'])}}
+    {{Form::close()}}
+@stop
