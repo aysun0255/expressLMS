cd1528bfa230206dc6f5713b3457f768c4b36849
User <User@pc>
2014-01-03 20:14:04 +0200

    new add

--- app/views/courses/enrol.blade.php
+++ app/views/courses/enrol.blade.php
@@ -3,7 +3,12 @@
 @section('content')
 <div class="well">
     <div class="container">
-       Enrol me in this course
+        <center> 
+            {{Form::open(array('route' => array('courses.enrol', $course->id),'method'=>'post','style' =>'display:inline;'))}}
+                {{Form::input('hidden','course_id',$course->id)}}
+                {{Form::submit('Enrol me in this course ',['type' => 'button','class' => 'btn btn-info','onclick' => "return confirm('Are you sure?');"])}}
+            {{Form::close()}} 
+        </center>
     </div>
 </div>
 @stop
