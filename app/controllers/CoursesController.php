072b0e2f5b708a22a61c8635a5cefb139bc94663
User <User@pc>
2014-01-03 12:21:21 +0200

    new add

--- app/controllers/CoursesController.php
+++ app/controllers/CoursesController.php
@@ -28,7 +28,7 @@ class CoursesController extends BaseController {
     public function show($id) {
         //show the selected course page
         $course = $this->course->whereId($id)->first();
-        return View::make('courses.show',['course' => $course]);
+        return View::make('courses.show', ['course' => $course]);
     }
 
     public function create() {
@@ -60,7 +60,35 @@ class CoursesController extends BaseController {
     }
 
     public function edit($id) {
-        //edit course details
+        //show user edit form
+        //get logged user usergroup and check for usergroup permissions
+        $usergroupId = Auth::user()->usergroup_id;
+        $canEditCourses = Usergroup::whereId($usergroupId)->first()->can_edit_courses;
+        if ($canEditCourses == 'yes') {
+            // Locate the model and store it in a variable:
+            $course = $this->course->whereId($id)->first();
+            // Then pass it to view:   
+            return View::make('courses.edit')->with(compact('course'));
+        } else {
+            //Return page with error message
+            return View::make('pages.message', ['error' => 'You don\'t have permission to edit this course!']);
+        }
+    }
+
+    public function update($id) {
+        //Get info for user permission for editing a course
+        $user = new User;
+        $canEditCourse = $user->whereId(Auth::user()->id)->first()->usergroup->can_edit_courses;
+        //Check for users permission to edit course
+        if ($canEditCourse == 'yes') {
+            $course = $this->course->whereId($id)->first();
+            $course->name = Input::get('name');
+            $course->description = Input::get('description');
+            $course->save();
+            return View::make('pages.message', ['success' => 'Your changes have been saved!']);
+        } else {
+            return View::make('pages.message', ['error' => 'You dont have permission to edit this course!']);
+        }
     }
 
 }
