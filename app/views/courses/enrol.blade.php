15c4b6814e2329839b066bcdbf3b7ae26be0b4bc
User <User@pc>
2014-01-04 09:48:57 +0200

    new add

--- app/views/courses/enrol.blade.php
+++ app/views/courses/enrol.blade.php
@@ -4,13 +4,21 @@
 <div class="well">
     <div class="container">
         <center> 
+            @if($course->allow_self_enrolment == 'yes')
             {{Form::open(array('route' => array('courses.enrol', $course->id),'method'=>'post','style' =>'display:inline;'))}}
                 @if($course->use_key == 'yes')
-                    {{Form::label('key','Enrolment key:'),Form::input('text','key')}}
+                    {{Form::label('enrolment_key','Enrolment key:'),Form::input('text','enrolment_key')}}
+                     {{$errors->first();}}
+                    </br>
                 @endif
                 {{Form::input('hidden','course_id',$course->id)}}
                 {{Form::submit('Enrol me in this course ',['type' => 'button','class' => 'btn btn-info','onclick' => "return confirm('Are you sure?');"])}}
             {{Form::close()}} 
+            @else
+            You can not enrol yourself in this course.
+            </br>
+            <a href="{{route('home')}}">Continue</a>
+            @endif
         </center>
     </div>
 </div>
