9fcb77b1c8cd4dcc99369c38ef6e76af2d7f01c2
aysun0255 <aysun025@abv.bg>
2014-03-05 21:17:16 +0200

    н

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -219,7 +219,7 @@ tinymce.init({
 
                     <a href="http://jigsaw.w3.org/css-validator/check/referer">
                         <img style="border:0;width:88px;height:31px"
-                             src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+                             src="http://www.explosetraffic.com/images/css.png"
                              alt="Valid CSS!" />
                     </a></p>
             </div>
